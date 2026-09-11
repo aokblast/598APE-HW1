a064175fd6c834ff33fcfcfc46a1eacb57fee8a2
ShengYi Hung <aokblast@FreeBSD.org>
2026-09-11 18:43:50 -0500

    vector: optimize solveScalers function
    On modern CPU, fp multiplication is much faster than fp divident.
    a / denom = a * (1 / denom). Replace this function and reusing 1 / denom
    to speed up

--- src/vector.cpp
+++ src/vector.cpp
@@ -80,13 +80,17 @@ Vector Vector::normalize(){
    return Vector(x/m, y/m, z/m); 
 }
 
-  
+/*
+ * Calculate coefficients using Cramer's Rule
+ * C = [v1, v2, v3] * v
+ */
 Vector solveScalers(Vector v1, Vector v2, Vector v3, Vector C){
    double denom = v1.z*v2.y*v3.x-v1.y*v2.z*v3.x-v1.z*v2.x*v3.y+v1.x*v2.z*v3.y+v1.y*v2.x*v3.z-v1.x*v2.y*v3.z;
    double a = C.z*v2.y*v3.x-C.y*v2.z*v3.x-C.z*v2.x*v3.y+C.x*v2.z*v3.y+C.y*v2.x*v3.z-C.x*v2.y*v3.z;
    double b = -C.z*v1.y*v3.x+C.y*v1.z*v3.x+C.z*v1.x*v3.y-C.x*v1.z*v3.y-C.y*v1.x*v3.z+C.x*v1.y*v3.z;
    double c = C.z*v1.y*v2.x-C.y*v1.z*v2.x-C.z*v1.x*v2.y+C.x*v1.z*v2.y+C.y*v1.x*v2.z-C.x*v1.y*v2.z;
-   return Vector(a/denom, b/denom, c/denom);
+   double denom_inv = 1 / denom;
+   return Vector(a*denom_inv, b*denom_inv, c*denom_inv);
 }
 
 Ray::Ray(const Vector& po, const Vector& ve): point(po), vector(ve){}
