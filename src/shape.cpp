447d19b6826ca69f742e642b7de681d9945a832b
WeihangLong <laukuralong@gmail.com>
2026-09-11 22:38:13 -0500

    Move TODO and review notes to local changes

--- src/shape.cpp
+++ src/shape.cpp
@@ -56,6 +56,7 @@ void calcColor(unsigned char* toFill,Autonoma* c, Ray ray, unsigned int depth){
    size_t seen = 0;
 
    while(t!=NULL){
+      //TODO: BOX::getIntersection
       double time = t->data->getIntersection(ray);
       //TODO: pre-allocate for 2^n size and then realloc if needed, instead of malloc/free every time`
       TimeAndShape *times2 = (TimeAndShape*)malloc(sizeof(TimeAndShape)*(seen + 1));
@@ -72,11 +73,13 @@ void calcColor(unsigned char* toFill,Autonoma* c, Ray ray, unsigned int depth){
 
    if (seen == 0 || times[0].time == inf) {
       double opacity, reflection, ambient;
+      //REVIEW: mag() in normalize(): NO
       Vector temp = ray.vector.normalize();
       const double x = temp.x;
       const double z = temp.z;
       const double me = (temp.y<0)?-temp.y:temp.y;
       const double angle = atan2(z, x);
+      //REVIEW: to check image::geColor() for potential optimization: NO
       c->skybox->getColor(toFill, &ambient, &opacity, &reflection, fix(angle/M_TWO_PI),fix(me));
       return;
    }
@@ -91,6 +94,7 @@ void calcColor(unsigned char* toFill,Autonoma* c, Ray ray, unsigned int depth){
 
    // TODO: Vectorize
    double lightData[3];
+   // TODO: check the getNormal()
    getLight(lightData, c, intersect, curShape->getNormal(intersect), curShape->reversible());
    toFill[0] = (unsigned char)(toFill[0]*(ambient+lightData[0]*(1-ambient)));
    toFill[1] = (unsigned char)(toFill[1]*(ambient+lightData[1]*(1-ambient)));
