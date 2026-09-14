c9bb382ee4a548a7f20c87078c98333a9303e80f
aokblast <aokblast@FreeBSD.org>
2026-09-14 14:19:49 -0500

    vector: Inline all one line function (#15)
    These simple arithematic operation can be inlined for better performance

--- src/vector.h
+++ src/vector.h
@@ -11,32 +11,86 @@
 class Vector{
 public:
   double x, y, z;
-  Vector(double a, double b, double c);
-  
-  
-  void operator +=(const Vector);
-  void operator -= (const Vector);
-  void operator *= (const double);
-  void operator *= (const float);
-  void operator *= (const int);
-  void operator /= (const double);
-  void operator /= (const float);
-  void operator /= (const int);
-  
-  Vector operator + (const Vector);
-  Vector operator - (const Vector);
-/*  Vector operator * (const Vector);*/
-  Vector operator * (const double);
-  Vector operator * (const float);
-  Vector operator * (const int);
-  Vector operator / (const double);
-  Vector operator / (const float);
-  Vector operator / (const int);
-  Vector cross(const Vector a);
-  double mag2();
-  double mag();
-  double dot(const Vector a);
-  Vector normalize();
+  Vector(double a, double b, double c) : x(a), y(b), z(c) {}
+  inline void operator-=(const Vector rhs) {
+    x -= rhs.x;
+    y -= rhs.y;
+    z -= rhs.z;
+  }
+  inline void operator+=(const Vector rhs) {
+    x += rhs.x;
+    y += rhs.y;
+    z += rhs.z;
+  }
+  inline void operator*=(const double rhs) {
+    x *= rhs;
+    y *= rhs;
+    z *= rhs;
+  }
+  inline void operator*=(const float rhs) {
+    x *= rhs;
+    y *= rhs;
+    z *= rhs;
+  }
+  inline void operator*=(const int rhs) {
+    x *= rhs;
+    y *= rhs;
+    z *= rhs;
+  }
+  inline void operator/=(const double rhs) {
+    x /= rhs;
+    y /= rhs;
+    z /= rhs;
+  }
+  inline void operator/=(const float rhs) {
+    x /= rhs;
+    y /= rhs;
+    z /= rhs;
+  }
+  inline void operator/=(const int rhs) {
+    x /= rhs;
+    y /= rhs;
+    z /= rhs;
+  }
+
+  inline Vector operator-(const Vector rhs) {
+    return Vector(x - rhs.x, y - rhs.y, z - rhs.z);
+  }
+  inline  Vector operator+(const Vector rhs) {
+    return Vector(x + rhs.x, y + rhs.y, z + rhs.z);
+  }
+  /*
+  Vector Vector::operator * (const Vector a) {
+     return Vector(y*a.z-z*a.y, z*a.x-x*a.z, x*a.y-y*a.x);
+  }*/
+  inline Vector operator*(const double rhs) {
+    return Vector(x * rhs, y * rhs, z * rhs);
+  }
+  inline Vector operator*(const float rhs) {
+    return Vector(x * rhs, y * rhs, z * rhs);
+  }
+  inline Vector operator*(const int rhs) {
+    return Vector(x * rhs, y * rhs, z * rhs);
+  }
+  inline Vector operator/(const double rhs) {
+    return Vector(x / rhs, y / rhs, z / rhs);
+  }
+  inline Vector operator/(const float rhs) {
+    return Vector(x / rhs, y / rhs, z / rhs);
+  }
+  inline Vector operator/(const int rhs) {
+    return Vector(x / rhs, y / rhs, z / rhs);
+  }
+  inline Vector cross(const Vector a) {
+    return Vector(y * a.z - z * a.y, z * a.x - x * a.z, x * a.y - y * a.x);
+  }
+  inline double mag2() { return x * x + y * y + z * z; }
+  inline double mag() { return sqrt(x * x + y * y + z * z); }
+  inline double dot(const Vector a) { return x * a.x + y * a.y + z * a.z; }
+  inline Vector normalize() {
+    double m = mag();
+    return Vector(x / m, y / m, z / m);
+  }
 } ;
 
 class Ray{
