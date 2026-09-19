cebc6dae48940ef9ed52bba78042cb240997d069
aokblast <aokblast@FreeBSD.org>
2026-09-19 05:05:32 +0000

    vector: Speedup solveScalers (#18)
    In previous observation, we found that it is probably that up, vect, and
    right are orthonormal. This means we can speedup Creamer rule with
    simple dot operation.
    
    After digging into the code, all of the derive class excepts for
    triangle are just rotation matrix. They are natually orthonormal.
    
    Triangle is much more tricky as it is the only one that forgets the
    angle mapping. When evaluating asin(sinval), it is actually signless as
    sin(150) == sin(30). As a solution, we use atan2 so that it consider the
    signness from sin and cos therefore get the correct angle.
    
    After that, we switch the solveScaler to simple dot operation and add an
    assertion check to make sure they are actually orthonormal.

--- src/vector.cpp
+++ src/vector.cpp
@@ -6,13 +6,14 @@
 //#include <printf.h>
 #include <stddef.h>
 #include "vector.h"
+#include <cassert>
 
+// We assume that v1, v2, and v3 are orthonormal.
 Vector solveScalers(Vector v1, Vector v2, Vector v3, Vector C){
-   double denom = v1.z*v2.y*v3.x-v1.y*v2.z*v3.x-v1.z*v2.x*v3.y+v1.x*v2.z*v3.y+v1.y*v2.x*v3.z-v1.x*v2.y*v3.z;
-   double a = C.z*v2.y*v3.x-C.y*v2.z*v3.x-C.z*v2.x*v3.y+C.x*v2.z*v3.y+C.y*v2.x*v3.z-C.x*v2.y*v3.z;
-   double b = -C.z*v1.y*v3.x+C.y*v1.z*v3.x+C.z*v1.x*v3.y-C.x*v1.z*v3.y-C.y*v1.x*v3.z+C.x*v1.y*v3.z;
-   double c = C.z*v1.y*v2.x-C.y*v1.z*v2.x-C.z*v1.x*v2.y+C.x*v1.z*v2.y+C.y*v1.x*v2.z-C.x*v1.y*v2.z;
-   return Vector(a/denom, b/denom, c/denom);
+  constexpr double THRESHOLD = 1e-9;
+  assert(std::fabs(v1.dot(v2)) <= THRESHOLD && std::fabs(v2.dot(v3)) <= THRESHOLD &&
+         std::fabs(v3.dot(v1)) <= THRESHOLD);
+  return Vector(C.dot(v1), C.dot(v2), C.dot(v3));
 }
 
 Ray::Ray(const Vector& po, const Vector& ve): point(po), vector(ve){}
