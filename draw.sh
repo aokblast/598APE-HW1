8bf7353a67de24354f37562215a770080ab7035a
aokblast <aokblast@FreeBSD.org>
2026-09-20 06:37:21 +0000

    script: Drop root requirement for the script (#20)

--- draw.sh
+++ draw.sh
@@ -34,27 +34,31 @@ if [ ! -d ./perf ]; then
 	git clone https://github.com/brendangregg/FlameGraph.git ./perf/fg
 fi
 
+EVT_AVAIL=`sysctl -n kernel.perf_event_paranoid`
+
+if [ ! "$EVT_AVAIL" -eq 1 ]; then
+	echo "Impossible to run in such security level. Try sysctl -w kernel.perf_event_paranoid=1"
+	exit 1
+fi
+
 make
 
 if [ "$PIANOROOM" = true ]; then
-	sudo perf record -F 99 -g -o ./perf/pianoroom.data -- ./main.exe -i inputs/pianoroom.ray --ppm -o output/pianoroom.ppm -H 500 -W 500
-	sudo chmod o+r ./perf/pianoroom.data
+	perf record -F max -g -o ./perf/pianoroom.data -- ./main.exe -i inputs/pianoroom.ray --ppm -o output/pianoroom.ppm -H 500 -W 500
 	perf script -i ./perf/pianoroom.data > ./perf/pianoroom.perf
 	perf/fg/stackcollapse-perf.pl ./perf/pianoroom.perf  > ./perf/pianoroom.folded
 	perf/fg/flamegraph.pl ./perf/pianoroom.folded > ./perf/pianoroom.svg
 fi
 
 if [ "$GLOBE" = true ]; then
-	sudo perf record -F 99 -g -o ./perf/globe.data -- ./main.exe -i inputs/globe.ray --ppm  -a inputs/globe.animate --movie -F 24
-	sudo chmod o+r ./perf/globe.data
+	perf record -F max -g -o ./perf/globe.data -- ./main.exe -i inputs/globe.ray --ppm  -a inputs/globe.animate --movie -F 24
 	perf script -i ./perf/globe.data > ./perf/globe.perf
 	perf/fg/stackcollapse-perf.pl ./perf/globe.perf  > ./perf/globe.folded
 	perf/fg/flamegraph.pl ./perf/globe.folded > ./perf/globe.svg
 fi
 
 if [ "$ELEPHANT" = true ]; then
-	sudo perf record -F 99 -g -o ./perf/elephant.data  -- ./main.exe -i inputs/elephant.ray --ppm  -a inputs/elephant.animate --movie -F 24 -W 100 -H 100 -o output/sphere.mp4
-	sudo chmod o+r ./perf/elephant.data
+	perf record -F max -g -o ./perf/elephant.data  -- ./main.exe -i inputs/elephant.ray --ppm  -a inputs/elephant.animate --movie -F 24 -W 1000 -H 1000 -o output/sphere.mp4
 	perf script -i ./perf/elephant.data > ./perf/elephant.perf
 	perf/fg/stackcollapse-perf.pl ./perf/elephant.perf  > ./perf/elephant.folded
 	perf/fg/flamegraph.pl ./perf/elephant.folded > ./perf/elephant.svg
